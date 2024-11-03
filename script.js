ad325e899284a77d0f2244624b138d5af57cb34f
Rohan Mr <130464410+rohanmr551@users.noreply.github.com>
2024-11-03 15:30:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function colorchange(){
+    let red=Math.floor(Math.random()*256)
+    let blue=Math.floor(Math.random()*256)
+    let green=Math.floor(Math.random()*256)
+    let color = "rgb(" + red + "," + green + "," + blue + ")"
+    document.getElementById("colorflipper").style.background=color
+}
